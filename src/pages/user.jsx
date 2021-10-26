21a4d500778865e2b38f9777b55a76516d2043ac
Alex Saxton <alex.saxton98@gmail.com>
2021-10-26 14:36:12 +0100

    Added CSS and Song, Playlist and Input components

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,10 +1,59 @@
+import './user.css';
+
 export const User = () =>{
     return(
-        <div>
-        <h2>Playlist</h2>
+        <div className="container">
+            <Playlist />
+            
         </div>
     )
 }
+
+const Playlist = () => {
+    return(
+        <div className="playlist">
+            <Input/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            <Song/>
+            
+        </div>
+    )
+}
+
+const Input = () => {
+    return(
+    <>
+        <label>Playlist Name</label>
+        <input
+            className ="input"
+            type="text"    
+
+        />
+    </>
+    )
+}
+
+const Song = () => {
+    return(
+        <div className="song">
+            <p className="songInfo">Song Title</p>
+            <p className="songInfo">Album</p>
+            <p className="songInfo">Artist</p>
+        </div>
+    )
+}
+
 
 
 export default User;
