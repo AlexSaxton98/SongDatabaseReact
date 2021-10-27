eebe8e516a11ab4afeca5ad534017f513d6cf6d3
Jonny <j.p.sharrock1@gmail.com>
2021-10-27 15:07:41 +0100

    Added login page + react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-
-import logo from './logo.svg';
+import Home from './pages/home';
+import user from './pages/user'
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/user' component={user} />
+    </Switch>
     </div>
   );
 }
